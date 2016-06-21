f154018c47032b2ad0b3bda2d43459350bb64d3d
Damir Sh. Yakupov <yds@bk.ru>
2016-06-21 16:59:47 +0400

    svnsetlog.sh - identation changed

--- src/svnsetlog.sh
+++ src/svnsetlog.sh
@@ -10,18 +10,21 @@
 : ${pathSVN:=/opt/dat/svn}
 
 udfMain() {
- local r
- #
- #
- [[ -n "$1"          ]] || eval $(udfOnError throw iErrorEmptyOrMissingArgument)
- [[ -d ${pathSVN}/$1 ]] || eval $(udfOnError throw iErrorNoSuchFileOrDir "${pathSVN}/$1")
- #
- for r in $(ls -rt r*); do
-  echo "$1 - setlog $r"
-  svnadmin setlog ${pathSVN}/$1 -r ${r/r/} --bypass-hooks $r
- done
+	local r
+#
+	[[ -n "$1"          ]] || eval $(udfOnError throw iErrorEmptyOrMissingArgument)
+	[[ -d ${pathSVN}/$1 ]] || eval $(udfOnError throw iErrorNoSuchFileOrDir "${pathSVN}/$1")
+#
+	for r in $(ls -rt r*); do
+
+		echo "$1 - setlog $r"
+		svnadmin setlog ${pathSVN}/$1 -r ${r/r/} --bypass-hooks $r
+
+	done
+
 }
 #
 #
 #
 udfMain "$@"
+#
