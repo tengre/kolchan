79fd867739b27b158e7386875446e5f14873b917
Damir Sh. Yakupov <yds@bk.ru>
2016-06-21 16:57:25 +0400

    svnlog2revs.sh - added handling of the second argument
    - second argument enables a note about the number of SVN revision if
      named as 'add-svn-rev'

--- src/svnlog2revs.sh
+++ src/svnlog2revs.sh
@@ -11,51 +11,51 @@ udfMain() {
   sed -i -r -e "s/\t/%SVNLOG2REVSTABULA%/g" -e "s/^(\s+)/%SVNLOG2REVSIDENT%\1/" $fn
 #
   while read s; do
-  
+
     [[ "$s" =~ ^------------------------------------------------------------------------$ ]] && continue
     if [[ "$s" =~ ^(r[0-9]+)[[:space:]]\|[[:space:]][[:graph:]]+[[:space:]]\|[[:space:]](.*)[[:space:]]\(.*\).*[0-9]+.line(s)?$ ]]; then
-    
+
       if [[ -n "$fnRev" && -f "$fnRev" && -n "$tsRev" ]]; then
-      
-#	printf -- "--\n--\t(svn rev %s)\n" "$fnRev" >> $fnRev
+
+	[[ "$2" == "add-svn-rev" ]] && printf -- "--\n--\t(svn rev %s)\n" "$fnRev" >> $fnRev
 	sed -i -r -e "s/%SVNLOG2REVSTABULA%/\t/g" -e "s/%SVNLOG2REVSIDENT%//" $fnRev
 	touch --date="$tsRev" $fnRev
-	
+
       fi
-      
+
       if [[ -n "${BASH_REMATCH[1]}" ]]; then
-      
+
 	fnRev="${BASH_REMATCH[1]}"
 	tsRev="${BASH_REMATCH[2]}"
 	rm -f $fnRev
 	echo "$fnRev $tsRev"
-	
+
       else
-      
+
 	eval $(udfOnError throw iErrorUnexpected '$s')
-	
+
       fi
-      
+
       read s
       continue
-      
+
     fi
-    
+
     [[ "$fnRev" =~ ^r[0-9]+$ ]] || continue
     echo "$s" >> $fnRev
-    
+
   done < $fn
 
   if [[ -n "$fnRev" && -f "$fnRev" && -n "$tsRev" ]]; then
-      
-#    printf -- "--\n--\t(svn rev %s)\n" "$fnRev" >> $fnRev
+
+	[[ "$2" == "add-svn-rev" ]] && printf -- "--\n--\t(svn rev %s)\n" "$fnRev" >> $fnRev
     sed -i -r -e "s/%SVNLOG2REVSTABULA%/\t/g" -e "s/%SVNLOG2REVSIDENT%//" $fnRev
     touch --date="$tsRev" $fnRev
 
   fi
-  
+
   sed -i -r -e "s/%SVNLOG2REVSTABULA%/\t/g" -e "s/%SVNLOG2REVSIDENT%//" $fn
-  
+
 }
 #
 #
