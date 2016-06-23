7f494edbea84795f7e01b73bca9145f1eb4ab695
Damir Sh. Yakupov <yds@bk.ru>
2016-06-23 12:44:15 +0400

    fixed calculation of the total number of commits
    - calculated total count of commits from stdout of 'git shortlog -s'
    - added checking external commands 'bc' and 'xargs'
    - 'git add ...' is performed only after a successful batch editing
    - revision number (count of commits) incremented anticipating the
      expected commit
    - improved code - rolled condition of git status

--- src/gitadd.sh
+++ src/gitadd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# $Id: gitadd.sh 7 2016-06-22 15:57:47+04:00 toor $
+# $Id: gitadd.sh 10 2016-06-23 12:41:05+04:00 toor $
 #
 . bashlyk
 #
@@ -8,32 +8,32 @@
 #
 udfMain() {
 
-	udfThrowOnCommandNotFound date git grep sed
+	udfThrowOnCommandNotFound bc date git grep sed xargs
 
-	local a=( $( git shortlog -s ) ) fn ic ts ui
+	local fmt fn ic ts ui
+
 	fmt='sed -i -r -e "s/%sId(.*)?%s/%s/" %s'
+	ic=$( git shortlog -s | grep -oP '^\s+\d+\s+' | xargs | tr ' ' '+' | bc )
 
 	eval set -- "$(_ sArg)"
 
-	if [[ -f "$1" && -n "$( git status -s "$1" | grep -P "^[ AM?][M?]\s$1" )" ]]; then
+	[[ -d "$1" ]] && eval $( udfOnError exitecho iErrorNonValidArgument "this version do not support directories, only files \($1\)..." )
 
-		ts="$( date --rfc-3339=s )"
-		ic="${a[0]}"
-		fn="${1##*/}"
-		ui="$USER"
+	[[ -f "$1" && -n "$( git status -s "$1" | grep -P "^[ AM?][M?]\s$1" )" ]] || {
 
-		if udfWarnOnEmptyVariable fn ic ts ui; then
+		eval $(udfOnError retwarn iErrorNonValidArgument "$1 is mask, not exist or not modified, but may be indexed...")
 
-			s="$( printf '\044Id: %s %d %s %s \044\n' "$fn" "$ic" "$ts" "$ui" )"
-			eval "$( printf "$fmt" '\\\$' '\\\$' '$s' '$fn')"
+	}
 
-		fi
+	fn="${1##*/}"
+	ic=$(( ic + 1 ))
+	ts="$( date --rfc-3339=s )"
+	ui="$(_ sUser)"
 
-		git add "$1"
+	if udfWarnOnEmptyVariable fn ic ts ui; then
 
-	else
-
-		echo "$1 not modified, but may be indexed..."
+		s="$( printf '\044Id: %s %d %s %s \044\n' "$fn" "$ic" "$ts" "$ui" )"
+		eval "$( printf "$fmt" '\\\$' '\\\$' '$s' '$fn')" && git add "$1"
 
 	fi
 
